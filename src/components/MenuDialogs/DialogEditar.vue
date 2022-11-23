42566e4f58b9084cfb0b4248b9f2bdae13f4105e
Flavio Lucio <flaviolucio10@gmail.com>
2022-11-23 15:30:27 -0300

    criando dialog editar

--- src/components/MenuDialogs/DialogEditar.vue
+++ src/components/MenuDialogs/DialogEditar.vue
@@ -5,7 +5,7 @@
       persistent
       max-width="290"
     >
-      <template v-slot:activator="{ on, attrs }">
+      <!-- <template v-slot:activator="{ on, attrs }">
         <v-btn
           color="primary"
           dark
@@ -14,27 +14,37 @@
         >
           Open Dialog
         </v-btn>
-      </template>
+      </template> -->
       <v-card>
         <v-card-title class="text-h5">
-          Use Google's location service?
+          Editar
         </v-card-title>
-        <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+        
+        <v-card-text>Edite a sua tarefa</v-card-text>
+        <v-col
+          cols="12"
+
+        >
+          <v-text-field
+            label="Tarefa"
+            outlined
+          ></v-text-field> <!--ele adicionou aqui o atributo class= "px-3" para fazer um efeito similar ao cols="12"-->
+        </v-col>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn
-            color="green darken-1"
+            color="red darken-1"
             text
             @click="dialog = false"
           >
-            Disagree
+            Cancelar
           </v-btn>
           <v-btn
-            color="green darken-1"
+            color="teal darken-2"
             text
             @click="dialog = false"
           >
-            Agree
+            Editar
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -44,9 +54,10 @@
 
 <script>
   export default {
+    props: ['dialogo'],
     data () {
       return {
-        dialog: true,
+        dialog: this.dialogo,
       }
     },
   }
--- src/components/Tarefas/TarefaMenu.vue
+++ src/components/Tarefas/TarefaMenu.vue
@@ -16,14 +16,15 @@
         <v-list-item
           v-for="(item, index) in menuTarefa"
           :key="index"
-          @click="item.AoClicar()"
-        >
+          @click= "testando()"                
+        >   <!--"item.AoClicar()" -->
          <v-icon left :color="`${item.color} darken-1`">{{item.icon}}</v-icon>
           <v-list-item-title> <span :class="`${item.color}--text text--darken-1`"> {{ item.title }}  </span></v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
-        <DialogEditar/>
+        <DialogEditar v-if="teste" :dialogo="teste"/>  <!--parei aqui, melhorar isso -->
+
     </div>
 </template>
 
@@ -38,8 +39,14 @@ import DialogEditar from "../MenuDialogs/DialogEditar.vue";
   }
 })
 export default class TarefaMenu extends Vue{
-     public menuTarefa = [{title:"Excluir",icon:"mdi-trash-can",color:"red",AoClicar(){console.log("teste")}},
+    public teste = false;
+    public menuTarefa = [{title:"Excluir",icon:"mdi-trash-can",color:"red",ativarDialog:false,AoClicar(){this.ativarDialog=true}},
                        {title:"Editar",icon:"mdi-pencil",color:"green",AoClicar(){console.log("teste2")}}]
+
+    testando()
+    {
+      this.teste = true;
+    }
 }
 </script>
 
